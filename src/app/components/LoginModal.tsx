c9bc4475ada50181c82ca4a5dce425a2250cf440
Abdulaziz <i@abdaziz.dev>
2024-01-04 14:59:45 +0300

    Lock the Sign in button, then send the email

--- src/app/components/LoginModal.tsx
+++ src/app/components/LoginModal.tsx
@@ -5,50 +5,49 @@ import { signIn } from "next-auth/react";
 
 
 function LoginModal({ opened, onClose, disableClose }: { opened: boolean; onClose: () => void; disableClose: boolean; }) {
-	const [pressedSignIn, setPressedSignIn] = useState(false);
-	const form = useForm({
-		initialValues: {
-			email: '',
-		},
-		validate: {
-			email: (value) =>
-				/^[^\s@]+@stu\.kau\.edu\.sa$/.test(value)
-					? null
-					: 'Invalid email, please enter a valid KAU email address.',
-		},
-	});
+  const [pressedSignIn, setPressedSignIn] = useState(false);
+  const form = useForm({
+    initialValues: {
+      email: '',
+    },
+    validate: {
+      email: (value) =>
+        /^[^\s@]+@stu\.kau\.edu\.sa$/.test(value)
+          ? null
+          : 'Invalid email, please enter a valid KAU email address.',
+    },
+  });
 
 
-	const handleSignIn = (email: string) => {
-		void signIn("email", { email, redirect: false }).then(() => {
-			setPressedSignIn(true)
-		});
-	}
+  const handleSignIn = (email: string) => {
+    setPressedSignIn(true)
+    void signIn("email", { email, redirect: false })
+  }
 
-	return (
-		<Modal opened={opened} onClose={onClose} title="Log in" centered overlayProps={{ backgroundOpacity: 0.55, blur: 3, }}
-		trapFocus={true} closeOnEscape={disableClose ? false : !pressedSignIn}
-		closeOnClickOutside={disableClose ? false : !pressedSignIn}
-		withCloseButton={disableClose ? false : !pressedSignIn}
-		>
-			<form onSubmit={form.onSubmit((values) => handleSignIn(values.email))}>
-				<Text size="md" style={{ marginBottom: '20px' }}>
-					In order to use this website, you must sign in with your university email.
-				</Text>
-				<TextInput withAsterisk label="Email" {...form.getInputProps('email')} />
+  return (
+    <Modal opened={opened} onClose={onClose} title="Log in" centered overlayProps={{ backgroundOpacity: 0.55, blur: 3, }}
+      trapFocus={true} closeOnEscape={disableClose ? false : !pressedSignIn}
+      closeOnClickOutside={disableClose ? false : !pressedSignIn}
+      withCloseButton={disableClose ? false : !pressedSignIn}
+    >
+      <form onSubmit={form.onSubmit((values) => handleSignIn(values.email))}>
+        <Text size="md" style={{ marginBottom: '20px' }}>
+          In order to use this website, you must sign in with your university email.
+        </Text>
+        <TextInput withAsterisk label="Email" {...form.getInputProps('email')} />
 
-				<Group justify="center" mt="md">
-					<Button type="submit" disabled={pressedSignIn}>Submit</Button>
-				</Group>
+        <Group justify="center" mt="md">
+          <Button type="submit" disabled={pressedSignIn}>Submit</Button>
+        </Group>
 
-				{pressedSignIn && (
-					<Text size="sm" style={{ marginTop: '10px', textAlign: 'center' }}>
-						Please check your email to sign in. If you don&apos;t see the email, please check your spam folder.
-					</Text>
-				)}
-			</form>
-		</Modal>
-	);
+        {pressedSignIn && (
+          <Text size="sm" style={{ marginTop: '10px', textAlign: 'center' }}>
+            Please check your email to sign in. If you don&apos;t see the email, please check your spam folder.
+          </Text>
+        )}
+      </form>
+    </Modal>
+  );
 }
 
 export default LoginModal;
