7917d3aeee394169edc87b6873f71582117f1b62
Abdulaziz <me@abdaziz.dev>
2025-08-25 13:18:59 +0300

    fix: allow 3-character course codes in group creation

--- src/app/api/groups/route.ts
+++ src/app/api/groups/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
   try {
     // Define a schema for validating the incoming request
     const schema = z.object({
-      courseCode: z.string().max(4).min(4),
+      courseCode: z.string().min(3).max(4),
       courseNumber: z.number().min(101).max(499),
       section: z.string().min(1).max(3).optional(),
       groupLink: z.string().url().max(64).refine((value) => value.startsWith("https://chat.whatsapp.com/"), { message: "Invalid group link" }),
